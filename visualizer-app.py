9d09b63f2ada2c76a09b56ea8dc556ebf388052e
kyshGulab <122585945+kyshGulab@users.noreply.github.com>
2024-04-21 13:32:41 -0400

    Update visualizer-app.py

--- visualizer-app.py
+++ visualizer-app.py
@@ -22,6 +22,7 @@ if uploaded_file is not None:
 
 api_key = "sk-proj-fB8SWqk8dMxXjRwzGdFQT3BlbkFJjt2Zd2yenJe3GMzNFnqH"
 openai.api_key = api_key
+OpenAI.api_key = os.getenv('api_key')
 
 question = st.text_area("How do you want to see your data")
 
