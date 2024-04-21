1dc739be9064eb3e9198dec4c98481da3dd7d151
kyshGulab <122585945+kyshGulab@users.noreply.github.com>
2024-04-21 11:41:20 -0400

    Update visualizer-app.py

--- visualizer-app.py
+++ visualizer-app.py
@@ -7,5 +7,9 @@ uploaded_file = st.file_uploader("Upload CSV file", type=["csv"])
 
 if uploaded_file is not None:
   df = pd.read_csv(uploaded_file)
-  st.write("Uploaded DataFrame:")
-  st.write(df)
+  column_names = df.columns.tolist()
+  selected_columns = st.multiselect("Select columns to display", column_names)
+  if selected_columns:
+    df_selected = df[selected_columns]
+    st.write("Selected DataFrame:")
+    st.write(df_selected)
